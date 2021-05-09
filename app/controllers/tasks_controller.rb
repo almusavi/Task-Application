6a1ae7274a260ffb9624b4e130aeccb4464b9672
al-musavi <amusavi@rjreliance.com>
2021-05-09 09:52:41 -0500

    refactor resources and remove unnecessary methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,7 @@ class TasksController < ApplicationController
 		@tasks = Task.all
 	end
 
-	def show
-	end
-
-	def new
-	end
-
+#custom method to mark tas complete 
 	def toggle_is_done
 		@task = Task.find(params[:id])
 		@task.is_done = true
@@ -20,29 +15,19 @@ class TasksController < ApplicationController
 		 end
 	end
 
-	def toggle_is_done
-		@task = Task.find(params[:id])
-		@task.is_done = true
-		@task.save
-		respond_to do |format|
-		    format.html { redirect_to root_path }
-		    format.js { }
-		 end
-	end
-
+#create method to create new task
 	def create
 		@task = Task.new(task_params)
 		@task.save
-		
 		respond_to do |format|
 		    format.html { redirect_to root_path }
 		    format.js { }
 		  end
 	end
 
+#destroy method to delete unwanted task
 	def destroy
 		@task = Task.find params[:task]
-	
 		@task.destroy
 		respond_to do |format|
 			format.html { redirect_to root_path }
